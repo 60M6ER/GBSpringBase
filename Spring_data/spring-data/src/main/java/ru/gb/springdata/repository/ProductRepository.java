95e68a2d224d6211a16dbe9463db9b4b4fad0503
Boris Larionov <79999987388@ya.ru>
2023-03-20 02:25:39 +0300

    complite

---
+++ Spring_data/spring-data/src/main/java/ru/gb/springdata/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package ru.gb.springdata.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.gb.springdata.model.Product;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByPriceGreaterThanEqual(Double price);
+    List<Product> findAllByPriceLessThanEqual(Double price);
+    List<Product> findAllByPriceGreaterThanEqualAndPriceLessThanEqual(Double minPrice, Double maxPrice);
+
+
+}
---
+++ Spring_data/spring-data/src/main/java/ru/gb/springdata/service/InitService.java
@@ -0,0 +1,31 @@
+package ru.gb.springdata.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import ru.gb.springdata.model.Product;
+import ru.gb.springdata.repository.ProductRepository;
+
+@Service
+public class InitService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initProducts() {
+        productRepository.deleteAll();
+        for (int i = 1; i < 21; i++) {
+            productRepository.save(
+                    new Product("Product " + i,
+                    Double.parseDouble(String.format("%.2f", 1 + Math.random() * 99).replace(",", "."))
+                    )
+            );
+        }
+    }
+}
